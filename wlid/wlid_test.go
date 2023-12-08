8d05ad46c91bf091b1c8bc86b6ae4988947d9bef
Matthias Bertschy <matthias.bertschy@gmail.com>
2023-12-08 08:34:04 +0100

    do not truncate wlid if namespace is empty, use string builder
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- wlid/wlid_test.go
+++ wlid/wlid_test.go
@@ -1,6 +1,7 @@
 package wlid
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -25,5 +26,53 @@ func TestSpiffeSIDInfoSuccess(t *testing.T) {
 	if er != nil || ms.Level0 != "HipsterShopCluster2" || ms.Level0Type != "cluster" || ms.Level1 != "dev" || ms.Level1Type != "namespace" ||
 		ms.Kind != "secret" || ms.Name != "caregcred" {
 		t.Errorf("TestSpiffeSIDInfoSuccess failed to parse %v", SID)
+	}
+}
+
+func Test_generateWLID(t *testing.T) {
+	type args struct {
+		pLevel0 string
+		level0  string
+		pLevel1 string
+		level1  string
+		k       string
+		name    string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "k8s wlid",
+			args: args{
+				pLevel0: ClusterWlidPrefix,
+				level0:  "HipsterShopCluster2",
+				pLevel1: NamespaceWlidPrefix,
+				level1:  "prod",
+				k:       "Deployment",
+				name:    "cartservice",
+			},
+			want: "wlid://cluster-HipsterShopCluster2/namespace-prod/deployment-cartservice",
+		},
+		{
+			name: "k8s wlid no namespace",
+			args: args{
+				pLevel0: ClusterWlidPrefix,
+				level0:  "HipsterShopCluster2",
+				pLevel1: NamespaceWlidPrefix,
+				level1:  "",
+				k:       "ClusterRoleBinding",
+				name:    "cartservice",
+			},
+			want: "wlid://cluster-HipsterShopCluster2/namespace-/clusterrolebinding-cartservice",
+			// FIXME: do we want "wlid://cluster-HipsterShopCluster2/clusterrolebinding-cartservice" instead?
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := generateWLID(tt.args.pLevel0, tt.args.level0, tt.args.pLevel1, tt.args.level1, tt.args.k, tt.args.name)
+			assert.Equal(t, tt.want, got)
+		})
 	}
 }
