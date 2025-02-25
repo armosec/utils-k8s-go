bafd4b1bbd51d52a053e45fcea6ec4880a524373
Matthias Bertschy <matthias.bertschy@gmail.com>
2025-02-25 08:56:00 +0100

    add ContinuousPostureScan to cluster config
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- armometadata/armometadata.go
+++ armometadata/armometadata.go
@@ -17,12 +17,13 @@ const (
 )
 
 type ClusterConfig struct {
-	ClusterName         string `json:"clusterName"`         // cluster name defined manually or from the cluster context
-	AccountID           string `json:"accountID"`           // use accountID instead of customerGUID
-	GatewayWebsocketURL string `json:"gatewayWebsocketURL"` // in-cluster gateway component websocket url
-	GatewayRestURL      string `json:"gatewayRestURL"`      // in-cluster gateway component REST API url
-	KubevulnURL         string `json:"kubevulnURL"`         // in-cluster kubevuln component REST API url
-	KubescapeURL        string `json:"kubescapeURL"`        // in-cluster kubescape component REST API url
+	ClusterName           string `json:"clusterName"`         // cluster name defined manually or from the cluster context
+	AccountID             string `json:"accountID"`           // use accountID instead of customerGUID
+	GatewayWebsocketURL   string `json:"gatewayWebsocketURL"` // in-cluster gateway component websocket url
+	GatewayRestURL        string `json:"gatewayRestURL"`      // in-cluster gateway component REST API url
+	KubevulnURL           string `json:"kubevulnURL"`         // in-cluster kubevuln component REST API url
+	KubescapeURL          string `json:"kubescapeURL"`        // in-cluster kubescape component REST API url
+	ContinuousPostureScan bool   `json:"continuousPostureScan"`
 	armotypes.InstallationData
 }
 
